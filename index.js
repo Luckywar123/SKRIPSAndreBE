0701a9ce192d6d2ebfc7c5011ef85b29aa3228d2
M Luthfi Taufiqurrahman <mluthfitaufiq@gmail.com>
2023-08-04 23:39:34 +0700

    Add get transaction and history

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 8000;
@@ -12,24 +12,24 @@ app.use(cors());
 
 // MySQL Connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'inventorydb', // Replace with your database name
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "inventory-a3", // Replace with your database name
 });
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log('Connected to the MySQL database');
+  console.log("Connected to the MySQL database");
 });
 
 // Fetch products from the database
-app.get('/products', (req, res) => {
-  const query = 'SELECT * FROM products';
+app.get("/products", (req, res) => {
+  const query = "SELECT * FROM products";
   connection.query(query, (err, results) => {
     if (err) {
-      console.error('Error fetching products:', err);
-      res.status(500).json({ error: 'Error fetching products' });
+      console.error("Error fetching products:", err);
+      res.status(500).json({ error: "Error fetching products" });
     } else {
       res.json({ data: results });
     }
@@ -37,13 +37,13 @@ app.get('/products', (req, res) => {
 });
 
 // Fetch skus for a specific product
-app.get('/products/:idBarang/skus', (req, res) => {
+app.get("/products/:idBarang/skus", (req, res) => {
   const idBarang = req.params.idBarang;
-  const query = 'SELECT * FROM skus WHERE idBarang = ?';
+  const query = "SELECT * FROM skus WHERE idBarang = ?";
   connection.query(query, [idBarang], (err, results) => {
     if (err) {
-      console.error('Error fetching skus:', err);
-      res.status(500).json({ error: 'Error fetching skus' });
+      console.error("Error fetching skus:", err);
+      res.status(500).json({ error: "Error fetching skus" });
     } else {
       if (results.length === 0) {
         // If no skus found for the given product ID, return an empty array
@@ -56,7 +56,7 @@ app.get('/products/:idBarang/skus', (req, res) => {
 });
 
 // Route to fetch return items with product and SKU details
-app.get('/return-items', (req, res) => {
+app.get("/return-items", (req, res) => {
   const query = `
     SELECT r.id, r.jumlah, r.alasan, p.nama AS nama_barang, s.skuCode
     FROM return_items r
@@ -65,8 +65,8 @@ app.get('/return-items', (req, res) => {
   `;
   connection.query(query, (err, results) => {
     if (err) {
-      console.error('Error fetching return items:', err);
-      res.status(500).json({ error: 'Error fetching return items' });
+      console.error("Error fetching return items:", err);
+      res.status(500).json({ error: "Error fetching return items" });
     } else {
       res.json({ data: results });
     }
@@ -74,64 +74,112 @@ app.get('/return-items', (req, res) => {
 });
 
 // Add a new product or update an existing product
-app.post('/products', (req, res) => {
+app.post("/products", (req, res) => {
   const newProduct = req.body;
   if (!newProduct.idBarang) {
     // If idBarang is not provided, it's a new product, so INSERT into products table
-    const insertQuery = 'INSERT INTO products SET ?';
+    const insertQuery = "INSERT INTO products SET ?";
     connection.query(insertQuery, newProduct, (err, results) => {
       if (err) {
-        console.error('Error adding new product:', err);
-        res.status(500).json({ error: 'Error adding new product' });
+        console.error("Error adding new product:", err);
+        res.status(500).json({ error: "Error adding new product" });
       } else {
         const insertedProduct = { ...newProduct, idBarang: results.insertId };
-        res.json({ message: 'Product added successfully', data: insertedProduct });
+        res.json({
+          message: "Product added successfully",
+          data: insertedProduct,
+        });
       }
     });
   } else {
     // If idBarang is provided, it's an existing product, so UPDATE the product in products table
     const idBarang = newProduct.idBarang;
     delete newProduct.idBarang; // Remove the idBarang from the newProduct object
-    const updateQuery = 'UPDATE products SET ? WHERE idBarang = ?';
+    const updateQuery = "UPDATE products SET ? WHERE idBarang = ?";
     connection.query(updateQuery, [newProduct, idBarang], (err, results) => {
       if (err) {
-        console.error('Error updating product:', err);
-        res.status(500).json({ error: 'Error updating product' });
+        console.error("Error updating product:", err);
+        res.status(500).json({ error: "Error updating product" });
       } else {
-        res.json({ message: 'Product updated successfully', data: newProduct });
+        res.json({ message: "Product updated successfully", data: newProduct });
       }
     });
   }
 });
 
 // Route untuk menambahkan data pengembalian baru
-app.post('/return-items', (req, res) => {
+app.post("/return-items", (req, res) => {
   const { idBarang, idSKU, jumlah, alasan } = req.body;
 
   // Periksa apakah SKU yang diberikan valid
-  const checkSkuQuery = 'SELECT * FROM skus WHERE idSKU = ?';
+  const checkSkuQuery = "SELECT * FROM skus WHERE idSKU = ?";
   connection.query(checkSkuQuery, [idSKU], (err, skuResults) => {
     if (err) {
-      console.error('Error checking SKU:', err);
-      res.status(500).json({ error: 'Error checking SKU' });
+      console.error("Error checking SKU:", err);
+      res.status(500).json({ error: "Error checking SKU" });
       return;
     }
 
     if (skuResults.length === 0) {
       // Jika idSKU tidak valid, kembalikan pesan kesalahan
-      res.status(404).json({ message: 'Invalid SKU' });
+      res.status(404).json({ message: "Invalid SKU" });
     } else {
       // Tambahkan data pengembalian ke dalam tabel return_items
-      const insertQuery = 'INSERT INTO return_items (idBarang, idSKU, jumlah, alasan) VALUES (?, ?, ?, ?)';
-      connection.query(insertQuery, [idBarang, idSKU, jumlah, alasan], (err, result) => {
-        if (err) {
-          console.error('Error adding return item:', err);
-          res.status(500).json({ error: 'Error adding return item' });
-        } else {
-          const insertedItem = { id: result.insertId, idBarang, idSKU, jumlah, alasan };
-          res.json({ message: 'Return item added successfully', data: insertedItem });
+      const insertQuery =
+        "INSERT INTO return_items (idBarang, idSKU, jumlah, alasan) VALUES (?, ?, ?, ?)";
+      connection.query(
+        insertQuery,
+        [idBarang, idSKU, jumlah, alasan],
+        (err, result) => {
+          if (err) {
+            console.error("Error adding return item:", err);
+            res.status(500).json({ error: "Error adding return item" });
+          } else {
+            const insertedItem = {
+              id: result.insertId,
+              idBarang,
+              idSKU,
+              jumlah,
+              alasan,
+            };
+            res.json({
+              message: "Return item added successfully",
+              data: insertedItem,
+            });
+          }
         }
-      });
+      );
+    }
+  });
+});
+
+app.get("/transaction", (req, res) => {
+  const query = `
+    SELECT * FROM transaksi
+  `;
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error fetching return items:", err);
+      res.status(500).json({ error: "Error fetching return items" });
+    } else {
+      res.json({ data: results });
+    }
+  });
+});
+
+app.get("/history", (req, res) => {
+  const query = `
+    SELECT p.idBarang, h.tanggal, p.nama, h.jumlah, s.skuCode, h.status
+    FROM history h
+    LEFT JOIN skus s ON h.idSKU = s.idSKU
+    LEFT JOIN products p ON h.idBarang = p.idBarang
+  `;
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error fetching return items:", err);
+      res.status(500).json({ error: "Error fetching return items" });
+    } else {
+      res.json({ data: results });
     }
   });
 });
